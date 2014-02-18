b2d9524804e5050ab4a9bb410a9fb1ded1822f08
Ruben Vermeersch <ruben@rocketeer.be>
2014-02-18 12:16:14 +0100

    Clean up things.
    Changes:
    
    * Moved the formats out of the task definition (they'll never change and
      can be moved out). Make the name more concise. Use `javascript` as the
      indicator for the JS target.
    * Rename `outputFormat` to `format`.
    * Revert `moduleName` to `module`: this is an externally used name and
      we can't just change it. People have this in their configurations, no
      breakage allowed.

--- tasks/compile.js
+++ tasks/compile.js
@@ -1,37 +1,36 @@
 var po = require('pofile');
 
+var formats = {
+    'javascript': {
+        addLocale: function (locale, strings) {
+            return "    gettextCatalog.setStrings('" + locale + "', " + (JSON.stringify(strings)) + ");\n";
+        },
+        format: function (locales, options) {
+            return "angular.module(\"" + options.module + "\").run(['gettextCatalog', function (gettextCatalog) {\n" + locales.join('') + "\n}]);";
+        }
+    },
+    'json': {
+        addLocale: function (locale, strings) {
+            return {locale: locale, strings: strings};
+        },
+        format: function (locales, options) {
+            return JSON.stringify(locales);
+        }
+    }
+};
+
 module.exports = function (grunt) {
     grunt.registerMultiTask('nggettext_compile', 'Compile strings from .po files', function () {
-        var outputFormatters = {
-            'module': {
-                addLocale: function(locale, strings) {
-                    return "    gettextCatalog.setStrings('" + locale + "', " + (JSON.stringify(strings)) + ");\n";
-                },
-                format: function(locales, options) {
-                    return "angular.module(\"" + options.moduleName +
-                        "\").run(['gettextCatalog', function (gettextCatalog) {\n" + locales.join('') + "\n}]);";
-                }
-            },
-            'json': {
-                addLocale: function(locale, strings) {
-                    return {locale: locale, strings: strings};
-                },
-                format: function(locales, options) {
-                    return JSON.stringify(locales);
-                }
-            }
-        };
-
         var options = this.options({
-            outputFormat: 'module',
-            moduleName: 'gettext'
+            format: 'javascript',
+            module: 'gettext'
         });
 
-        if (!outputFormatters.hasOwnProperty(options.outputFormat)) {
-            throw new Error('There is no "' + options.outputFormat + '" output format.');
+        if (!formats.hasOwnProperty(options.format)) {
+            throw new Error('There is no "' + options.format + '" output format.');
         }
 
-        var outputFormatter = outputFormatters[options.outputFormat];
+        var format = formats[options.format];
 
         this.files.forEach(function (file) {
             var locales = [];
@@ -50,10 +49,10 @@ module.exports = function (grunt) {
                     strings[item.msgid] = item.msgstr.length === 1 ? item.msgstr[0] : item.msgstr;
                 }
 
-                locales.push(outputFormatter.addLocale(catalog.headers.Language, strings));
+                locales.push(format.addLocale(catalog.headers.Language, strings));
             });
 
-            grunt.file.write(file.dest, outputFormatter.format(locales, options));
+            grunt.file.write(file.dest, format.format(locales, options));
         });
     });
 };
