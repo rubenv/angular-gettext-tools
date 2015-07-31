884fd9192952a22c864b6d91f49772f61f30637b
Yannik Hampe <yzeugs@cipher-code.de>
2015-07-31 14:59:55 +0200

    Simplify regex tests further by extracting more reoccuring code in a function

--- test/extract_regex.js
+++ test/extract_regex.js
@@ -19,6 +19,14 @@ function matchFilter(regex, filter) {
     return result;
 }
 
+function generateTest( name, txt, expected, regex ) {
+    it(name, function () {
+        var matches = matchFilter(regex || mkAttrRegex('{{', '}}'), txt);
+
+        assert.deepEqual(matches, expected);
+    });
+}
+
 describe('Extract: Filter regex', function () {
     var regex = null;
 
@@ -26,122 +34,49 @@ describe('Extract: Filter regex', function () {
         regex = mkAttrRegex('{{', '}}');
     });
 
-    it('Matches a simple string', function () {
-        var matches = matchFilter(regex, '{{\'Hello\'|translate}}');
+    generateTest('Matches a simple string', '{{\'Hello\'|translate}}', [ { msgid: 'Hello' } ]);
 
-        assert.deepEqual(matches, [ { msgid: 'Hello' } ]);
-    });
+    generateTest('Matches a simple string with multiple filters', '{{\'Hello\'|translate|lowercase}}', [ { msgid: 'Hello' } ]);
 
-    it('Matches a simple string with multiple filters', function () {
-        var matches = matchFilter(regex, '{{\'Hello\'|translate|lowercase}}');
+    generateTest('Matches double quotes', '{{"Hello"|translate}}', [ { msgid: 'Hello' } ]);
 
-        assert.deepEqual(matches, [ { msgid: 'Hello' } ]);
-    });
+    generateTest('Matches double quotes with multiple filters', '{{\'Hello\'|translate|lowercase}}', [ { msgid: 'Hello' } ]);
 
-    it('Matches double quotes', function () {
-        var matches = matchFilter(regex, '{{"Hello"|translate}}');
+    generateTest('Matches multiple strings', '{{\'Hello\'|translate}} {{"Second"|translate}}', [
+        { msgid: 'Hello' },
+        { msgid: 'Second' }
+    ]);
 
-        assert.deepEqual(matches, [ { msgid: 'Hello' } ]);
-    });
-
-    it('Matches double quotes with multiple filters', function () {
-        var matches = matchFilter(regex, '{{\'Hello\'|translate|lowercase}}');
-
-        assert.deepEqual(matches, [ { msgid: 'Hello' } ]);
-    });
-
-    it('Matches multiple strings', function () {
-        var matches = matchFilter(regex, '{{\'Hello\'|translate}} {{"Second"|translate}}');
-
-        assert.deepEqual(matches, [
+    generateTest('Matches multiple strings with multiple filters', '{{\'Hello\'|translate|lowercase}} {{"Second"|translate|uppercase}}', [
             { msgid: 'Hello' },
             { msgid: 'Second' }
         ]);
-    });
 
-    it('Matches multiple strings with multiple filters', function () {
-        var matches = matchFilter(regex, '{{\'Hello\'|translate|lowercase}} {{"Second"|translate|uppercase}}');
-
-        assert.deepEqual(matches, [
+    generateTest('Matches encoded quotes', '{{\'Hello\'|translate}} {{&quot;Second&quot;|translate}}', [
             { msgid: 'Hello' },
             { msgid: 'Second' }
         ]);
-    });
 
-    it('Matches encoded quotes', function () {
-        var matches = matchFilter(regex, '{{\'Hello\'|translate}} {{&quot;Second&quot;|translate}}');
-
-        assert.deepEqual(matches, [
+    generateTest('Matches encoded quotes with multiple filters', '{{\'Hello\'|translate}} {{&quot;Second&quot;|translate|lowercase}}', [
             { msgid: 'Hello' },
             { msgid: 'Second' }
         ]);
-    });
 
-    it('Matches encoded quotes with multiple filters', function () {
-        var matches = matchFilter(regex, '{{\'Hello\'|translate}} {{&quot;Second&quot;|translate|lowercase}}');
+    generateTest('Matches spaces', '{{ "Hello" | translate }}', [ { msgid: 'Hello' } ]);
 
-        assert.deepEqual(matches, [
-            { msgid: 'Hello' },
-            { msgid: 'Second' }
-        ]);
-    });
+    generateTest('Matches spaces with multiple filters', '{{ "Hello" | translate | lowercase }}', [ { msgid: 'Hello' } ]);
 
-    it('Matches spaces', function () {
-        var matches = matchFilter(regex, '{{ "Hello" | translate }}');
+    generateTest('Can customize delimiters', '[[\'Hello\'|translate]]', [ { msgid: 'Hello' } ], mkAttrRegex('[[', ']]'));
 
-        assert.deepEqual(matches, [ { msgid: 'Hello' } ]);
-    });
+    generateTest('Can customize delimiters with multiple filters', '[[\'Hello\'|translate|lowercase]]', [ { msgid: 'Hello' } ], mkAttrRegex('[[', ']]'));
 
-    it('Matches spaces with multiple filters', function () {
-        var matches = matchFilter(regex, '{{ "Hello" | translate | lowercase }}');
+    generateTest('Can be used without delimiters', '\'Hello\' | translate', [ { msgid: 'Hello' } ], mkAttrRegex('', ''));
 
-        assert.deepEqual(matches, [ { msgid: 'Hello' } ]);
-    });
+    generateTest('Can be used without delimiters with multiple filters', '\'Hello\' | translate | lowercase', [ { msgid: 'Hello' } ], mkAttrRegex('', ''));
 
-    it('Can customize delimiters', function () {
-        var regex = mkAttrRegex('[[', ']]');
-        var matches = matchFilter(regex, '[[\'Hello\'|translate]]');
+    generateTest('Ignores filters in default delimiters if using empty string as delimiters', '{{\'Hello\' | translate}}', [], mkAttrRegex('', ''));
 
-        assert.deepEqual(matches, [ { msgid: 'Hello' } ]);
-    });
+    generateTest('Matches string with context', '{{\'Hello\'|translate:\'someContext\'}}', [ { msgid: 'Hello', msgctxt: 'someContext' } ]);
 
-    it('Can customize delimiters with multiple filters', function () {
-        var regex = mkAttrRegex('[[', ']]');
-        var matches = matchFilter(regex, '[[\'Hello\'|translate|lowercase]]');
-
-        assert.deepEqual(matches, [ { msgid: 'Hello' } ]);
-    });
-
-    it('Can be used without delimiters', function () {
-        var regex = mkAttrRegex('', '');
-        var matches = matchFilter(regex, '\'Hello\' | translate');
-
-        assert.deepEqual(matches, [ { msgid: 'Hello' } ]);
-    });
-
-    it('Can be used without delimiters with multiple filters', function () {
-        var regex = mkAttrRegex('', '');
-        var matches = matchFilter(regex, '\'Hello\' | translate | lowercase');
-
-        assert.deepEqual(matches, [ { msgid: 'Hello' } ]);
-    });
-
-    it('Ignores filters in default delimiters if using empty string as delimiters', function () {
-        var regex = mkAttrRegex('', '');
-        var matches = matchFilter(regex, '{{\'Hello\' | translate}}');
-
-        assert.deepEqual(matches, []);
-    });
-
-    it('Matches string with context', function () {
-        var matches = matchFilter(regex, '{{\'Hello\'|translate:\'someContext\'}}');
-
-        assert.deepEqual(matches, [ { msgid: 'Hello', msgctxt: 'someContext' } ]);
-    });
-
-    it('Matches string with context and spaces', function () {
-        var matches = matchFilter(regex, '{{ \'Hello\' | translate : \'someContext\' }}');
-
-        assert.deepEqual(matches, [ { msgid: 'Hello', msgctxt: 'someContext' } ]);
-    });
+    generateTest('Matches string with context and spaces', '{{ \'Hello\' | translate : \'someContext\' }}', [ { msgid: 'Hello', msgctxt: 'someContext' } ]);
 });
