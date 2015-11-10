1ed25b8831b5997eac9543562b236785672a05ae
Liviu Nicolae <liviu.nicolae@jlg.ro>
2015-11-10 18:03:39 +0200

    Implement c# support to extract from .cs files the properties marked with _translate_ sufix and implement Translations.cs file generation containing a dictionary with all the translations. Translations.cs is generated when running the compile command and you can use it in c# by calling  Translations.Translate(key) method

---
+++ test/fixtures/Constants.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+
+
+namespace Core.Common
+{
+    public static class Constants
+    {
+
+        public static class Messages
+        {
+            public const string SuccessfullySaved_translate_       = 
+                  "Successfully saved!"           ; 
+            public const string OccurrencesDeleted_translate_ =            "The selected items were deleted successfully."   ;
+            public const string StatusUpdated = "Investigation status was updated successfully.";
+
+            public static class EmailGroupsMessages
+            {
+                public const string EmailGroupDeleted = "Email Group successfully deleted!";
+                public const string EmailUpdated = "Email successfully updated!";
+                public const string EmailDeleted = "Email successfully deleted!";
+            }
+        }
+      
+    }
+}
+
+
