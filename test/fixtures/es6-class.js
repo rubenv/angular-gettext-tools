fa10289378cac518cc8f61538bf7494118b7f326
Ondrej Zdych <zdych.ondrej@gmail.com>
2016-10-12 17:17:09 +0200

    feat: extend es6-class.js test suite

--- test/fixtures/es6-class.js
+++ test/fixtures/es6-class.js
@@ -1,5 +1,17 @@
 class TestController {
   constructor(gettext) {
     var myString = gettext("Hi from an ES6 class!");
+
+    this.bindedMethod = ::this.method;
+
+    const component = <Component attr={true} />;
+
+    const a = { a: 1 };
+    const b = { b: 2 };
+
+    const c = { ...a, ...b };
   }
+
+  @test
+  method () { }
 }
