0ab52fbca4a9f9739e4465b1595e49b340203a2a
Ivano Alvino <ivano.alvino@navvis.com>
2018-08-07 11:09:41 +0200

    Add unit tests for casted variables in ts
    In addition, refactor a bit in order to remove an if/else construct

--- test/fixtures/ts.ts
+++ test/fixtures/ts.ts
@@ -3,5 +3,6 @@ angular.module("myApp").controller("helloController", (gettext) => {
     var longString: string = gettext(`One
 Two
 Three`);
+    var castedVar: any = <any> gettext("Casted");
     gettext(); // Should be ignored.
 });
