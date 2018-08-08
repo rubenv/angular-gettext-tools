a4f3ea1069f8534970b0881c5ff40e4bb1646fff
Alexander Schuetz <fenvariel@googlemail.com>
2018-08-08 12:32:49 +0200

    created test for issue 188

---
+++ test/issue_188.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var assert = require('assert');
+var fs = require('fs');
+var Extractor = require('..').Extractor;
+var testExtract = require('./utils').testExtract;
+
+describe('Extract', function () {
+    it('Extract "constructor" from HTML', function () {
+        var files = [
+            'test/fixtures/issue_188.html'
+        ];
+        var catalog = testExtract(files);
+
+        assert.equal(catalog.items.length, 1);
+        assert.equal(catalog.items[0].msgid, 'constructor');
+        assert.equal(catalog.items[0].msgstr, '');
+        assert.deepEqual(catalog.items[0].references, ['test/fixtures/issue_188.html:1', 'test/fixtures/issue_188.html:2', 'test/fixtures/issue_188.html:3']);
+    });
+});
